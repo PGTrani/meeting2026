f29129274e7bc206650ce2b2a284c36369e0f7e0
AndreaValenziano <valenzianoa@gmail.com>
2026-04-16 15:23:21 +0200

    Update links, add "laboratori.html", and enhance styles for new action buttons

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,6 +19,7 @@ module.exports = merge(common, {
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
+        { from: 'laboratori.html', to: 'laboratori.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
